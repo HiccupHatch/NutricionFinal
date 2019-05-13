041bd39b3b4065265eef59c66e36ab233961671f
Aarón Morán Holguín <jaaronmoran@gmail.com>
2019-05-13 18:43:15 +0200

    Retrieves user information from realtime database

--- app/src/main/java/com/example/hiccup/nutricion/LoginActivity.java
+++ app/src/main/java/com/example/hiccup/nutricion/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.hiccup.nutricion;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,6 +25,15 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
+
 public class LoginActivity extends Activity {
 
     private static final String TAG="Google Test";
@@ -35,6 +46,8 @@ public class LoginActivity extends Activity {
 
     private GoogleSignInOptions gso;
     private GoogleSignInClient mGoogleSignInClient;
+
+    private SharedPreferences sharedpreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +89,33 @@ public class LoginActivity extends Activity {
     private void LoginUsuario() {
         String email = eEmail.getText().toString();
         String password = ePass.getText().toString();
+
+        sharedpreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString("email", email);
+        DatabaseReference dbRef =
+                FirebaseDatabase.getInstance().getReference()
+                        .child("usuario");
+
+        Query usuarios= dbRef.orderByChild("email").equalTo(email);
+
+        usuarios.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                    String dni= singleSnapshot.getKey();
+                    SharedPreferences.Editor editor = sharedpreferences.edit();
+                    editor.putString("dni", dni);
+                    String foto = singleSnapshot.child("foto").getValue().toString();
+                    editor.putString("foto", foto);
+                    editor.commit();
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e(TAG, "onCancelled", databaseError.toException());
+            }
+        });
 
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
--- app/src/main/java/com/example/hiccup/nutricion/MainActivity.java
+++ app/src/main/java/com/example/hiccup/nutricion/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.hiccup.nutricion;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedpreferences = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+
+        String foto = sharedpreferences.getString("foto", "");
+
+        System.out.println("32: "+foto);
     }
 }
